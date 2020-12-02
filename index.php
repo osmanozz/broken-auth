d17b7462595a0079c31883c0a20d04cd3c7710f3
osmanozz <2120601@talnet.nl>
2020-12-02 16:10:58 +0100

    Insufficient Logging & Monitoring

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// error message to be logged 
+
+
+$log_file = "./log.txt"; 
+$date = $time = date('m/d/y h:iA');
+$ip = $_SERVER['REMOTE_ADDR'];
+$log_message= " ";
+// logging error message to given log file 
+error_log($log_message, 3, $log_file); 
 
 session_start();  
  $host = "localhost";  
@@ -6,6 +18,7 @@ session_start();
  $password = "Osmanosman1.";  
  $database = "brokenauth";  
  $message = " ";
+ 
  try  
  {  
       $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);  
@@ -25,19 +38,26 @@ session_start();
                 if($count > 0)  
                 {  
                     $message = "logged in"; 
+                    $log_message = "The gebruiker geprobeerd in " . $date . " in te loggen met ip adres van " . $ip . " met correct username van " . $_POST["username"]. " the login is successfull! " . "\n" ;
+                    error_log($log_message, 3, $log_file); 
                 }  
                 else  
                 {  
-                     $message = 'Wrong Data';
-                     session_destroy();  
+                     $message = "Wrong data";
+                     $log_message = "The gebruiker geprobeerd in " . $date . " in te loggen met ip adres van " . $ip . " met incorrect username van " .$_POST["username"]. " the log in is unsuccesfull ". "\n" ; 
+                     error_log($log_message, 3, $log_file); 
+
                 }  
+            
            }
+          
           
         }
  
  catch(PDOException $error)  
  {  
       $message = $error->getMessage();  
+      error_log("Failed to connect to database!", 0);
  }  
 
 ?>
